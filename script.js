531341eaf838c8d5f6c9d700e77a006bc77733bc
nirom-labs-0x <magira1124@gmail.com>
2026-07-15 16:28:42 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', () => {
+    console.log("Clínica de los Vásquez - Portal web cargado.");
+    
+    // Aquí en el futuro se conectará con staff.json para cargar los médicos dinámicamente
+    // y con config.json para el carrusel de portadas.
+});
